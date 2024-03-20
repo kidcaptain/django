c42a367338082dfb324bcc4614d5992c0ecea186
hunterjul007 <93106837+hunterjul007@users.noreply.github.com>
2024-03-20 19:41:28 +0100

    update

--- dog_shelters/views.py
+++ dog_shelters/views.py
@@ -28,17 +28,7 @@ def et_list(request):
 
 def classe_detail(request, pk):
     classe = get_object_or_404(models.Classe, pk=pk)
-    etudiant = models.Etudiant.objects.all()
-    context = {'classe': classe, 'etudiant': etudiant}
+    etudiant = models.Etudiant.objects.filter(classe = classe)
+    context = {'classe': classe, 'etudiants': etudiant}
     return render(request, 'templates/classe_detail.html', context=context)
 
-def register(request):
-    if request.method == 'POST':
-        nom = request.POST.get('a')
-        prenom = request.POST.get('b')
-        dateNaiss = request.POST.get('c')
-        d = request.POST.get('d')
-        classe = get_object_or_404(models.Classe, pk=d)
-        user = models.Etudiant.objects.create_user(nom=nom, prenom=prenom, dateNaiss=dateNaiss, classe=classe)
-        redirect('dog_shelters:index')
-    return render(request, 'templates/classe_list.html')
